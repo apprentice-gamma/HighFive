761b07ad73b039ffc243aa046b81e6b0b4d4cb4f
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-04-20 16:15:38 -0400

    creating the api factory

---
+++ public/app/api.factory.js
@@ -0,0 +1,10 @@
+(function() {
+    angular.module('HighFive').factory('APIFactory', APIFactory);
+
+    function APIFactory() {
+    	var factory = {
+    		googlePlacesKey:"AIzaSyDsMqIQw9WVNS8HJxbqz7gPkte4LNju4nw"
+    	}
+    	return factory;
+    }
+})();
