9ac59f70de6ab966caad68529fce404a559263f2
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-04-20 16:47:05 -0400

    creating the categories factory & controller

---
+++ public/app/categories.controller.js
@@ -0,0 +1,8 @@
+
+
+
+		var quick = "&query=fast+food";
+    	var formal = "&query=fine+dining";
+    	var pizza = "&query=pizza";
+    	var barFood = "&query=bar";
+    	var healthy = "&query=healthy";
---
+++ public/app/categories.factory.js
@@ -0,0 +1,16 @@
+(function() {
+    angular.module('HighFive').factory('CategoriesFactory', CategoriesFactory);
+
+    function CategoriesFactory($http, APIFactory) {
+    	var url = "https://maps.googleapis.com/maps/api/place/textsearch/json?type=restaurant&radius=8046.72&location=";
+    	var location = "42.3353685,-83.04925";
+    	var key = APIFactory.googlePlacesKey;
+
+    	var factory = {
+    		getList:function(category){
+    			return $http.get(url+location+category+"key="+key);
+    		}
+    	}
+    	return factory;
+    }
+})();
