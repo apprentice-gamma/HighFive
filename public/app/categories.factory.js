767442acee4362b2487e481023f2f055fc80500c
Ivan <ivan.herndon@detroitlabs.com>
2015-04-21 13:49:43 -0400

    added google places library object to category factory

--- public/app/categories.factory.js
+++ public/app/categories.factory.js
@@ -1,15 +1,45 @@
 (function() {
     angular.module('HighFive').factory('CategoriesFactory', CategoriesFactory);
 
-    function CategoriesFactory($http, APIFactory) {
-    	var url = "https://maps.googleapis.com/maps/api/place/textsearch/json?type=restaurant&radius=8046.72&location=";
-    	var location = "42.3353685,-83.04925";
+    function CategoriesFactory() {
 
-    	var factory = {
-    		getList:function(category){
-    			return $http.get(url+location+category+"&key="+key );
-    		}
-    	}
+    	var factory = {};
+        factory.pizzaList = [];
+        factory.quickList = [];
+        factory.formalList = [];
+        factory.healthyList = [];
+        factory.barFoodList = [];
+
+        factory.initialize = function (googleTextSearch){
+            var searchLocation = new google.maps.LatLng(42.3353685,-83.04925);
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: searchLocation,
+                zoom: 15
+            });
+            
+            var request = {
+                location: searchLocation,
+                radius: '8046.72',
+                query: googleTextSearch,
+            };
+
+            var service = new google.maps.places.PlacesService(map);
+            service.textSearch(request, callback);
+
+            var tempArray = [];
+            function callback(results, status) {
+              if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    tempArray.push(results[i]);
+                  // console.log(results[i]);
+                  // createMarker(results[i]);
+                }
+              }
+            }
+            return tempArray
+            
+    },
     	return factory;
     }
 })();
