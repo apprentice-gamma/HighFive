44ed6f30b780f0be4ff76c697bc346874a8d6686
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-04-21 11:04:24 -0400

    creating basic anglar set up

--- public/app/api.factory.js
+++ public/app/api.factory.js
@@ -3,7 +3,7 @@
 
     function APIFactory() {
     	var factory = {
-    		googlePlacesKey:"AIzaSyDsMqIQw9WVNS8HJxbqz7gPkte4LNju4nw"
+    		
     	}
     	return factory;
     }
--- public/app/categories.controller.js
+++ public/app/categories.controller.js
@@ -1,8 +1,21 @@
+angular.module("HighFive").controller("CategoriesController", CategoriesController); 
+function CategoriesController($http, CategoriesFactory){
+		var vm = this;
+		vm.quick = "&query=fast+food";
+    	vm.formal = "&query=fine+dining";
+    	vm.pizza = "&query=pizza";
+    	vm.barFood = "&query=bar";
+    	vm.healthy = "&query=healthy";
+    	vm.listData = {};
 
+    	vm.getList = function(category){
+    		CategoriesFactory.getList(category).success(function(data){
+    			vm.listData = data;
+    		});
+    	}
+    	vm.getList(vm.quick);
 
+    	vm.service = new google.maps.places.PlacesService(map);
+		vm.service.textSearch(request, callback);
+}
 
-		var quick = "&query=fast+food";
-    	var formal = "&query=fine+dining";
-    	var pizza = "&query=pizza";
-    	var barFood = "&query=bar";
-    	var healthy = "&query=healthy";
