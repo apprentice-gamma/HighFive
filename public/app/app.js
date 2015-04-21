286eb1bd7dc7a1460d9ca7f7eb4466585f77550f
Ivan <ivan.herndon@detroitlabs.com>
2015-04-21 11:33:18 -0400

    adding routing to app

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,23 @@
 (function(){
-	angular.module('HighFive', []);
+	angular.module('HighFive', ['ngRoute'])
+	.config(RouteConfiguration);
+
+	function RouteConfiguration($routeProvider){
+		$routeProvider
+		.when('/', {
+			templateUrl: 'templates/landingpage.html',
+		})
+		.when('/categories', {
+			templateUrl: 'templates/categoriespage.html',
+		})
+		.when('/categories/:category', {
+			templateUrl: 'templates/categorydetail.html',
+		})
+		.when('/categories/:category/:id', {
+			templateUrl: 'templates/restaurantdetail.html',
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	}
 })();
